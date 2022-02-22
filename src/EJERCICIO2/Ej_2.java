0b9aa2d66b35ec082914ab0d12492bd7bf7b8199
Mario Herrero <marioherrero2003@gmail.com>
2022-02-22 11:32:34 +0100

    Ej_2

---
+++ src/EJERCICIO2/Ej_2.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package EJERCICIO2;
+
+/**
+ *
+ * @author mario
+ */
+public class Ej_2 {
+    public static void main(String[] args) {
+    int cuenta = 2;
+    int i =0;
+    while( i != 10 ){
+        cuenta = cuenta * 2;
+        System.out.println("la cuenta es:" + cuenta);
+        i++;
+    }
+    }
+}
---
+++ src/ejercicios/tema1/EJERCICIOSTEMA1.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicios.tema1;
+
+/**
+ *
+ * @author mario
+ */
+public class EJERCICIOSTEMA1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int cuenta = 2;
+        int i = 0;  
+        while (i != 10){
+            cuenta =  cuenta * 2;
+            System.out.println("la cuenta es:" + cuenta );
+            i++;
+        }
+    }
+}
